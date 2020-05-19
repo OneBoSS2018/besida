ebf5d54fcd62408ae8eb46c8f06cd9586e223f12
OneBoSS2018 <bob204981@gmail.com>
2020-05-19 12:08:25 +0300

    fix frontend

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ io.on('connection', socket => {
       .to(user.room)
       .emit(
         'message',
-        formatMessage(botName, `${user.username} has joined the chat`)
+        formatMessage(botName, `${user.username} приєднався в нашу бесіду)`)
       );
 
     // Send users and room info
@@ -58,7 +58,7 @@ io.on('connection', socket => {
     if (user) {
       io.to(user.room).emit(
         'message',
-        formatMessage(botName, `${user.username} has left the chat`)
+        formatMessage(botName, `${user.username} покинув нас(`)
       );
 
       // Send users and room info
